28fa2fba94e136a66076709197cf61b581dd8e3f
yangfei <yangfei@boqii.net>
2020-07-13 14:22:46 +0800

    feat: co 自动执行generator函数

---
+++ README.md
@@ -0,0 +1,12 @@
+# 原生 js
+
+- 数组各种 API
+- promise
+- co, async, await
+- 高级函数, curry, compose, bind,call,apply 等
+-
+
+参考
+
+- https://juejin.im/post/5cc175496fb9a0325031b6e3
+- https://juejin.im/post/5cef46226fb9a07eaf2b7516
---
+++ co.js
@@ -0,0 +1,34 @@
+function* test() {
+  let res = yield "hello"
+  console.log(res)
+  let res1 = yield "world"
+  console.log(res1)
+  return res1
+}
+
+// let t = test()
+
+// console.log(t.next())
+// console.log(t.next("value"))
+
+function co(fn) {
+  return new Promise((resolve, reject) => {
+    let it = fn()
+
+    function next(data) {
+      let { done, value } = it.next(data)
+      if (done) {
+        resolve(value)
+      } else {
+        Promise.resolve(value).then((data) => next(data))
+      }
+    }
+
+    next()
+  })
+}
+
+co(test).then((data) => {
+  console.log("最终结果")
+  console.log(data)
+})
